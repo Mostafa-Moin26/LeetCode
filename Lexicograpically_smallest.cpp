90ad5ddd2a2d85c63ef80d58c1ffe9ebcf8fc888
Mostafa Al Moin <sa479374@gmail.com>
2024-07-14 14:54:55 +0600

    optimization

--- Lexicograpically_smallest.cpp
+++ Lexicograpically_smallest.cpp
@@ -8,10 +8,10 @@ public:
     string getSmallestString(string s) {
         
         for (int i = 0; i < s.size() - 1; ++i) {
-            int a = static_cast<int>(s[i]);
-            int b = static_cast<int>(s[i + 1]);
+            int a = s[i] - '0';
+            int b = s[i] - '0';
             
-            if (a % 2 == b % 2 || a & 1 == b & 1) {
+            if (a % 2 == b % 2) {
                if (a > b) {
                    swap(s[i], s[i + 1]);
                    break;
