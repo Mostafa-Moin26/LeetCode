c10e83094cfe8b9763cd415245fa467598273020
Mostafa Al Moin <sa479374@gmail.com>
2024-07-31 09:39:52 +0600

    solution

---
+++ Search_Insert_position.cpp
@@ -0,0 +1,19 @@
+// Problem Link ---->
+https://leetcode.com/problems/search-insert-position/description/
+
+// solution ----->
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return left;
+    }
+};
