512c492d5135b2fc8441c743c71f335b715ac1a8
Mostafa Al Moin <sa479374@gmail.com>
2024-08-10 07:59:24 +0600

    using hashmap

--- Happy_number.cpp
+++ Happy_number.cpp
@@ -34,3 +34,31 @@ public:
         return fast == 1;
     }
 };
+
+// using hashmap
+class Solution {
+public:
+    
+     int getSquareSum(int n) {
+        int currSum = 0;
+
+        while(n != 0) {
+
+            int remainder = (n % 10);
+            currSum += (remainder * remainder);
+            n /= 10;
+        }
+        return currSum;
+    }
+    bool isHappy(int n) {
+
+        unordered_set<int> st;
+
+        while (n != 1 && st.find(n) == st.end()) {
+            st.insert(n);
+            n = getSquareSum(n);
+        }
+        
+        return n == 1;
+    }
+};
