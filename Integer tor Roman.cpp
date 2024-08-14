c3cde714c4cef65eb4b2e55ae46625d05e1fb6b0
Mostafa Al Moin <sa479374@gmail.com>
2024-08-14 10:16:43 +0600

    using loop

--- Integer tor Roman.cpp
+++ Integer tor Roman.cpp
@@ -51,3 +51,28 @@ public:
         
     }
 };
+
+// using loop
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s = "";
+        vector<int> a = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> st = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        int i = 0;
+        while (num > 0) {
+
+            if (num >= a[i]) {
+                s += st[i];
+                num -= a[i];
+            } else {
+                i++;
+            }
+        }
+
+
+        return s;
+        
+    }
+};
