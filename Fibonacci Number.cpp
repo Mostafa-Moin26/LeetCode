92430fe38620315bef651e79813a8db895a35efd
Mostafa Al Moin <sa479374@gmail.com>
2024-10-15 14:05:00 +0600

    more solutions added

--- Fibonacci Number.cpp
+++ Fibonacci Number.cpp
@@ -10,3 +10,62 @@ public:
         return fib(n - 1) + fib(n - 2);
     }
 };
+
+// using dp Tabulation method
+class Solution {
+public:
+    int dp[32];
+
+    int fib(int n) {
+        
+        dp[0] = 0, dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+
+// using dp Memoization method
+class Solution {
+public:
+    int dp[32];
+
+    int fib(int n) {
+        
+        if (n <= 1) return n;
+
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        int res = fib(n - 1) + fib(n - 2);
+
+        return res;
+    }
+};
+
+// space optimization
+class Solution {
+public:
+
+    int fib(int n) {
+        
+        if (n <= 1) return n;
+
+        int res = 0;
+        int first = 0, second = 1;
+
+        for (int i = 2; i <= n; ++i) {
+
+            res = first + second;
+            first = second;
+            second = res;
+        }
+
+        return res;
+    }
+};
