18795b31d7b19471399288c57472c4b4c17979df
Mostafa Al Moin <sa479374@gmail.com>
2024-10-15 14:57:12 +0600

    solution added

--- Find_the_encripted_string.cpp
+++ Find_the_encripted_string.cpp
@@ -15,3 +15,20 @@ public:
         return res;
     }
 };
+
+// Beats 100%
+class Solution {
+public:
+    string getEncryptedString(string s, int k) {
+        
+        string res = "";
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+
+            res += s[(i + k) % n];
+        }
+
+        return res;
+    }
+};
