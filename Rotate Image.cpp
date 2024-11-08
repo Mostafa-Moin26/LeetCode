512a08670027de871c417b5d5b539862f7ae7216
Mostafa Al Moin <sa479374@gmail.com>
2024-11-08 19:44:05 +0600

    one more code added

--- Rotate Image.cpp
+++ Rotate Image.cpp
@@ -17,6 +17,21 @@ public:
     }
 };
 
+// little bit optimize
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> tmp = matrix;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = tmp[n - j - 1][i];
+            }
+        }
+    }
+};
+
 // not using another vector
 class Solution {
 public:
