f1abffced9aeda70d3a6566ac035e1ae3ee34cd8
Mostafa Al Moin <sa479374@gmail.com>
2025-07-12 10:08:07 +0600

    Better solution added

--- Median of two sorted Arrays.cpp
+++ Median of two sorted Arrays.cpp
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
 // Solutions ---->
+// Brute force
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -22,5 +23,58 @@ public:
         }
 
         return ans;
+    }
+};
+
+//Better, O(1) space 
+class Solution {
+public:
+
+    double merge(vector<int> &a, vector<int> &b) {
+        int n1 = a.size();
+        int n2 = b.size();
+        int idx2 = (n1 + n2) / 2;
+        int idx1 = idx2 - 1;
+        int el1 = -1, el2 = -1;
+        int i = 0, j = 0;
+        int cnt = 0;
+
+        while (i < n1 && j < n2) {
+
+            if (a[i] <= b[j]) {
+                if (cnt == idx1) el1 = a[i];
+                if (cnt == idx2) el2 = a[i];
+
+                i++, cnt++;
+            } else {
+                if (cnt == idx1) el1 = b[j];
+                if (cnt == idx2) el2 = b[j];
+
+                j++, cnt++;
+            }
+        }
+
+        while (i < n1) {
+            if (cnt == idx1) el1 = a[i];
+            if (cnt == idx2) el2 = a[i];
+            
+            i++, cnt++;
+        }
+        while (j < n2) {
+            if (cnt == idx1) el1 = b[j];
+            if (cnt == idx2) el2 = b[j];
+
+            j++, cnt++;
+        }
+
+        if ((n1 + n2) & 1) {
+            return el2;
+        }
+
+        return (el1 + el2) / 2.0;
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        return merge(nums1, nums2);
     }
 };
