22c53e92dabaafe5f8f10d8d0455548fbb9cd557
Mostafa Al Moin <sa479374@gmail.com>
2025-08-07 18:03:46 +0600

    more solutions added

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -34,4 +34,51 @@ public:
     }
 };
 
+// using map and stack
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> mapping = {{')', '('}, {']', '['}, {'}', '{'}};
 
+        for (char c : s) {
+            if (mapping.find(c) == mapping.end()) {
+                stack.push(c);
+            } else if (!stack.empty() && mapping[c] == stack.top()) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.empty();        
+    }
+};
+
+// using helper function and stack
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char cur : s) {
+            if (!st.empty()) {
+                char last = st.top();
+                if (isPair(last, cur)) {
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(cur);
+        }
+
+        return st.empty();        
+    }
+
+private:
+    bool isPair(char last, char cur) {
+        return (last == '(' && cur == ')') ||
+               (last == '{' && cur == '}') ||
+               (last == '[' && cur == ']');
+    }
+};    
