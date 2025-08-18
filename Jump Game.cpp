67d200139bfd99bafe0e69cb9ec223c44dda29bb
Mostafa Al Moin <sa479374@gmail.com>
2025-08-18 14:27:41 +0600

    optimal solution added on Jump Game

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -31,3 +31,23 @@ public:
         return false;
     }
 };
+
+// My optimal solution
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxJump = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            maxJump = max(maxJump, nums[i]);
+
+            if (maxJump == 0 && i != nums.size() - 1) {
+                return false;
+            }
+
+            maxJump--;
+        }
+
+        return true;
+    }
+};
