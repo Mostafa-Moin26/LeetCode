1749f00186038be0628fd8666c6ae099de537d02
Mostafa Al Moin <sa479374@gmail.com>
2025-08-19 11:06:04 +0600

    Optimal solution added on Jump Game II

--- Jump Game II.cpp
+++ Jump Game II.cpp
@@ -27,3 +27,25 @@ public:
         return jumps;
     }
 };
+
+// Little bit optimization
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int jumps = 0, currEnd = 0, maxReach = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+
+            maxReach = max(maxReach, i + nums[i]);
+
+            if (i == currEnd) {
+                jumps++;
+                currEnd = maxReach;
+            }
+        }
+
+        return jumps;
+
+    }
+};
