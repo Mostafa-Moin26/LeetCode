a4d451cbb54f4ca6a12e4b88c5e049e3a95fc386
Mostafa Al Moin <sa479374@gmail.com>
2026-02-22 10:43:23 +0600

    Flipping an Image

---
+++ Flipping an Image.cpp
@@ -0,0 +1,59 @@
+// Problem link ---->
+https://leetcode.com/problems/flipping-an-image/description/
+
+// Solutions ---->
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int rows = image.size();
+        int cols = image[0].size();
+
+        int l, r;
+
+        for (int i = 0; i < cols; i++) {
+            l = 0, r = cols - 1;
+
+            while (l <= r) {
+                swap(image[i][l], image[i][r]);
+                image[i][l] = image[i][l] ? 0 : 1;
+
+                if (l != r) {
+                    image[i][r] = image[i][r] ? 0 : 1;
+                }
+
+                l++, r--;
+            }
+        }
+
+        return image;
+    }
+};
+
+// using xor (^)
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int rows = image.size();
+        int cols = image[0].size();
+
+        int l, r;
+
+        for (int i = 0; i < cols; i++) {
+            l = 0, r = cols - 1;
+
+            while (l <= r) {
+                swap(image[i][l], image[i][r]);
+                image[i][l] ^= 1;
+
+                if (l != r) {
+                    image[i][r] ^= 1;
+                }
+
+                l++, r--;
+            }
+        }
+
+        return image;
+    }
+};
