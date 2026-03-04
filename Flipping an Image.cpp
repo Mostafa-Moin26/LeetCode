b7dbefa1d990f6656debeee76afc4a0bf3033386
Mostafa Al Moin <sa479374@gmail.com>
2026-03-04 10:16:50 +0600

    optimize solution added on Flipping an Image problem

--- Flipping an Image.cpp
+++ Flipping an Image.cpp
@@ -57,3 +57,20 @@ public:
         return image;
     }
 };
+
+// easy code using sort
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+
+        for (int i = 0; i < image.size(); i++) {
+
+            reverse(image[i].begin(), image[i].end());
+
+            for (int j = 0; j < image[0].size(); j++) {
+                image[i][j] = !image[i][j];
+            }
+        }
+        return image;
+    }
+};
