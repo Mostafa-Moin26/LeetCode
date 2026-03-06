5a00b8d9b1b1b0749d52d33fdd70a108981a3106
Mostafa Al Moin <sa479374@gmail.com>
2026-03-06 17:00:51 +0600

    another solution added on Next Permutation problem

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -40,3 +40,32 @@ public:
         }
     }
 };
+
+// also Optimized code
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int n = nums.size();
+        int i = n - 1;
+
+        while (i > 0 && nums[i - 1] >= nums[i]) {
+            i--;
+        }
+
+        if (i == 0) {
+            sort(nums.begin(), nums.end());
+            return;
+        } 
+
+        int j = n - 1;
+
+        while (j >= i && nums[j] <= nums[i - 1]) {
+            j--;
+        }
+
+        swap(nums[i - 1], nums[j]);
+        sort(nums.begin() + i, nums.end());
+        
+    }
+};
