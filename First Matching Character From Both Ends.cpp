3fd31d3c243e43d1f17f8a2a3fbc73fec93691de
Mostafa Al Moin <sa479374@gmail.com>
2026-03-29 10:37:28 +0600

    First Matching Character From Both Ends

---
+++ First Matching Character From Both Ends.cpp
@@ -0,0 +1,19 @@
+// Problem link --->
+https://leetcode.com/problems/first-matching-character-from-both-ends/description/
+
+// Solution --->
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+
+        int n = s.size(); 
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == s[n - i - 1]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
